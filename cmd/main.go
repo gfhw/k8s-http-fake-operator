2f4f27c62fb7465b7bc552b355563a048ee78563
ghhw <ghhw@users.noreply.github.com>
2026-03-29 10:59:35 +0800

    Remove unused WebApp controller
    The operator was registering a WebApp reconciler that is not needed for
    HTTPTestStub functionality. This was causing errors because the WebApp
    CRD was not installed. Removed WebApp controller registration to fix
    the error: 'no matches for kind WebApp in version httpteststub.example.com/v1'
    
    Only HTTPTestStub controller is now active.

--- cmd/main.go
+++ cmd/main.go
@@ -154,14 +154,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := (&controller.WebAppReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "WebApp")
-		os.Exit(1)
-	}
-
 	if err := (&controller.HTTPTestStubReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
